1b5beee75f27f84506a6e7a13fee0c43b10833be
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-18 00:47:46 +0600

    Style Added

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaCartPlus, FaSearch, FaStream, FaChevronRight, FaTelegramPlane } from "react-icons/fa";
-import { HiMail, HiPhone } from "react-icons/hi";
+import {    HiPhone } from "react-icons/hi";
 import './Navbar.css'
 import logo from "../../Assets/images/logo-blue.png"
 
@@ -20,18 +20,21 @@ const Navbar = () => {
 
                 {/* Top Navigation Sub bar */}
                 <div className='container hidden mx-auto p-3 text-[12px] md:flex items-center justify-between font-semibold text-gray-600'>
-                    <div className='flex items-center gap-2'>
+                    <div className='flex text-red-400 items-center gap-2'>
                         <FaTelegramPlane className='text-[15px]' />
                         <p>Express delivery and free returns within 7 days</p>
                     </div>
-                    <div className='md:flex justify-start gap-10 items-center'>
-                        <div className='flex items-center gap-2'>
-                            <HiMail className='text-[18px]' />
-                            <p>athleticshop@gmail.com</p>
-                        </div>
-                        <div className='flex items-center gap-2'>
+                    <div className='flex justify-around items-center gap-4 mr-20'> 
+                        <div className='flex items-center gap-2 text-red-400'>
                             <HiPhone className='text-[18px]' />
                             <p>+880 1310756941</p>
+                        </div>
+                        <div className='border-r-2 h-4 border-red-400'>
+
+                        </div>
+                        <div className='flex items-center gap-2 text-red-400'> 
+                            <Link to={'/login'}>Login</Link>
+                            <Link to={'/signup'}>Signup</Link>
                         </div>
                     </div>
                 </div>
@@ -41,7 +44,7 @@ const Navbar = () => {
 
                 {/* Navigation menu start */}
                 <nav className='container mx-auto nav-items-div'>
-                    <ul className="sidebar">
+                    <ul className="sidebar"> 
                         <li onClick={hideSidebar}><Link><FaChevronRight /></Link></li>
                         <li className='flex justify-center items-center text-gray-400 border-b-2 border-blue-200'>
                             <FaSearch className='hideOnMobile' />
@@ -60,7 +63,7 @@ const Navbar = () => {
                     </ul>
 
                     <ul>
-                        <li><img className="md:w-[180px] logo" src={logo} alt="" /></li>
+                        <li><img className="md:w-[150px]" src={logo} alt="" /></li>
                         <li className='hideOnMobile flex justify-center items-center text-gray-400 border-b-2 border-blue-600'>
                             <FaSearch />
                             <input className='outline-none p-2 w-[400px]' type="search" placeholder='Search your products' />
@@ -75,7 +78,7 @@ const Navbar = () => {
                                 Cart
                             </li>
                         </Link>
-                        <Link><li onClick={showSidebar} className='menu-icon text-[25px]'><FaStream /></li></Link>
+                        <Link><li onClick={showSidebar} className='menu-icon text-[25px]'><FaStream /></li></Link> 
                     </ul>
                 </nav>
             </div>
--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -11,34 +11,34 @@ const Header = () => {
         <>
             <Layout>
                 <section className="container mx-auto mt-10">
-                    <div className="md:w-1/4">
+                    <div className="md:w-[20%]">
                         <div className="flex items-center">
                             <HiLightningBolt className="text-2xl text-gray-600" />
                             <p className="text-4xl font-bold text-black">Categories</p>
                         </div>
-                        <ul className="md:mt-5 mt-2 gap-2 categories md:grid-cols-1 grid grid-cols-2">
+                        <ul className="md:mt-5 md:p-0 p-3 mt-2 gap-2 categories md:grid-cols-1 grid grid-cols-2">
 
-                            <Link className="flex items-center gap-2 bg-emerald-50	mt-2 rounded p-2">
+                            <Link className="flex items-center gap-2 bg-blue-25	mt-2 rounded p-2">
                                 <HiGift className="text-violet-400" />
                                 <li>Trending</li>
                             </Link>
-                            <Link className="flex mt-2 items-center gap-2 bg-emerald-50 rounded p-2">
+                            <Link className="flex mt-2 items-center gap-2 bg-blue-25 rounded p-2">
                                 <HiGift className="text-red-400" />
                                 <li>All Fashion</li>
                             </Link>
-                            <Link className="flex mt-2 items-center gap-2 bg-emerald-50 rounded p-2">
+                            <Link className="flex mt-2 items-center gap-2 bg-blue-25 rounded p-2">
                                 <HiGift  className="text-green-400"/>
                                 <li>Men Fashion</li>
                             </Link>
-                            <Link className="flex mt-2 items-center gap-2 bg-emerald-50 rounded p-2">
+                            <Link className="flex mt-2 items-center gap-2 bg-blue-25 rounded p-2">
                                 <HiGift className="text-yellow-600" />
                                 <li>Women Fashion</li>
                             </Link>
-                            <Link className="flex mt-2 items-center gap-2 bg-emerald-50 rounded p-2">
+                            <Link className="flex mt-2 items-center gap-2 bg-blue-25 rounded p-2">
                                 <HiGift className="text-purple-600" />
                                 <li>Kid's Fashion</li>
                             </Link>
-                            <Link className="flex mt-2 items-center gap-2 bg-emerald-50 rounded p-2">
+                            <Link className="flex mt-2 items-center gap-2 bg-blue-25 rounded p-2">
                                 <HiGift />
                                 <li>Accessories</li>
                             </Link>
