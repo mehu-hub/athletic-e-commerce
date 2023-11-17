16addbfd6ab707d48a70ecde01efc0fb600cfa9e
Mehedi Hasan <designwithmehedi@gmail.com>
2023-11-17 22:57:54 +0600

    Categories

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -11,33 +11,34 @@ const Header = () => {
         <>
             <Layout>
                 <section className="container mx-auto mt-10">
-                    <div>
+                    <div className="md:w-1/4">
                         <div className="flex items-center">
                             <HiLightningBolt className="text-2xl text-gray-600" />
                             <p className="text-4xl font-bold text-black">Categories</p>
                         </div>
-                        <ul className="p-3 md:mt-5 mt-2 categories md:grid-cols-1 grid grid-cols-2">
-                            <Link className="flex items-center gap-2">
+                        <ul className="md:mt-5 mt-2 gap-2 categories md:grid-cols-1 grid grid-cols-2">
+
+                            <Link className="flex items-center gap-2 bg-emerald-50	mt-2 rounded p-2">
                                 <HiGift className="text-violet-400" />
                                 <li>Trending</li>
                             </Link>
-                            <Link className="flex items-center md:mt-4 mt-2 gap-2">
+                            <Link className="flex mt-2 items-center gap-2 bg-emerald-50 rounded p-2">
                                 <HiGift className="text-red-400" />
                                 <li>All Fashion</li>
                             </Link>
-                            <Link className="flex items-center md:mt-4 mt-2 gap-2">
+                            <Link className="flex mt-2 items-center gap-2 bg-emerald-50 rounded p-2">
                                 <HiGift  className="text-green-400"/>
                                 <li>Men Fashion</li>
                             </Link>
-                            <Link className="flex items-center md:mt-4 mt-2 gap-2">
+                            <Link className="flex mt-2 items-center gap-2 bg-emerald-50 rounded p-2">
                                 <HiGift className="text-yellow-600" />
                                 <li>Women Fashion</li>
                             </Link>
-                            <Link className="flex items-center md:mt-4 mt-2 gap-2">
+                            <Link className="flex mt-2 items-center gap-2 bg-emerald-50 rounded p-2">
                                 <HiGift className="text-purple-600" />
                                 <li>Kid's Fashion</li>
                             </Link>
-                            <Link className="flex items-center md:mt-4 mt-2 gap-2">
+                            <Link className="flex mt-2 items-center gap-2 bg-emerald-50 rounded p-2">
                                 <HiGift />
                                 <li>Accessories</li>
                             </Link>
